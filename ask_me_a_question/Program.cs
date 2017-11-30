7e68c1116500a6257fa878bddac3cf5347279542
Dennis Luo <dennis.luo1@gmail.com>
2017-11-30 11:27:13 -0500

    Create name of program & add colors

--- ask_me_a_question/Program.cs
+++ ask_me_a_question/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace ask_me_a_question
 {
+    /// <summary>
+    /// Entry point for 'Ask Me a Question' program (By: Dennis Luo)
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            // Leave the original console text color
+            ConsoleColor origColor = Console.ForegroundColor;
+
+            programInfo();
+
+            // Original color 
+            Console.ForegroundColor = origColor;
+        }
+
+        /// <summary>
+        /// Print the name of program and author
+        /// To console
+        /// </summary>
+        static void programInfo()
+        {
+            // Console text colors for program name
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Ask ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Me ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("a ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Question ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("By: Dennis Luo ");
         }
     }
 }
