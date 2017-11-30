e6b68edbddec562ef6487f92dd37f88cb74d0b5f
Dennis Luo <dennis.luo1@gmail.com>
2017-11-30 13:43:06 -0500

    Implement static function to store GetAnswerFromUser function, need to fix bugs

--- ask_me_a_question/Program.cs
+++ ask_me_a_question/Program.cs
@@ -11,59 +11,25 @@ namespace ask_me_a_question
     /// </summary>
     class Program
     {
+        // static string theQuestion, theAnswer;
         static void Main(string[] args)
         {
             // Leave the original console text color
             ConsoleColor origColor = Console.ForegroundColor;
 
-            programInfo();
+            ProgramInfo();
 
             // Create random object to return randomize numbers
             Random randomObj = new Random();
 
             while (true)
             {
-                // Ask user for question and store in theQuestion variable
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("Ask Me a Question: ");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                string theQuestion = Console.ReadLine();
+                string theQuestion = GetQuestion();
 
-                if (theQuestion.ToLower() == "quit")
-                {
-                    break;
-                }
-
-                // Get random number
-                int randomNum = randomObj.Next(4);
-                
-                // Random number to display answer
-                switch(randomNum)
-                {
-                    case 0:
-                        {
-                            Console.WriteLine("YES!");
-                            break;
-                        }
-                    case 1:
-                        {
-                            Console.WriteLine("No!");
-                            break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("Definitely NO!");
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.WriteLine("You're awesome, YES!");
-                            break;
-                        }
-                }
+                GetAnswerFromUser(theQuestion);
             }
 
-
+            
             // Original color 
             Console.ForegroundColor = origColor;
         }
@@ -72,7 +38,7 @@ namespace ask_me_a_question
         /// Print the name of program and author
         /// To console
         /// </summary>
-        static void programInfo()
+        static void ProgramInfo()
         {
             // Console text colors for program name
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -87,7 +53,71 @@ namespace ask_me_a_question
             Console.WriteLine("By: Dennis Luo ");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Type 'quit' anytime to leave program");
-            
+
         }
+
+        // Will return text user types
+        static string GetQuestion()
+        {
+            // Ask user for question and store in theQuestion variable
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Ask Me a Question: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string theQuestion = Console.ReadLine();
+
+            return theQuestion;
+
+        }
+
+        // Get answer for user
+        static void  GetAnswerFromUser(string theQuestion)
+        {
+            if (theQuestion.Length == 0)
+            {
+                Console.WriteLine("Please type a question :) ");
+                return;
+            }
+            if (theQuestion.ToLower() == "quit")
+            {
+                break;
+            }
+
+            // Insults & slurs will exit program
+            if (theQuestion.ToLower() == "you suck")
+            {
+                Console.WriteLine("You suck even more! Exiting program...BYE");
+                break;
+            }
+            // Get random number
+            int randomNum = randomObj.Next(4);
+
+            // Random number to display answer
+            switch (randomNum)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("YES!");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("No!");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Definitely NO!");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("You're awesome, YES!");
+                        break;
+                    }
+            }
+
+
+        }
+
     }
 }
