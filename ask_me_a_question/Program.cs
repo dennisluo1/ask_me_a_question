a5991033ff69dd5a4436443e752cded5729bf7b7
Dennis Luo <dennis.luo1@gmail.com>
2017-11-30 16:28:22 -0500

    Update while loop, separate InsultSlur statement into static functions

--- ask_me_a_question/Program.cs
+++ ask_me_a_question/Program.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace ask_me_a_question
+namespace AskQuestion
 {
     /// <summary>
     /// Entry point for 'Ask Me a Question' program (By: Dennis Luo)
     /// </summary>
     class Program
     {
-        // static string theQuestion, theAnswer;
         static void Main(string[] args)
         {
             // Leave the original console text color
@@ -25,19 +25,66 @@ namespace ask_me_a_question
             while (true)
             {
                 string theQuestion = GetQuestion();
+                
+                // Pause for 1-2 seconds before giving answer
+                int secondsToSleep = randomObj.Next(2) + 1;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Hmmm...thinking of an answer...");
+                Thread.Sleep(secondsToSleep * 1000);
 
-                GetAnswerFromUser(theQuestion);
+                if (theQuestion.Length == 0)
+                {
+                    Console.WriteLine("Please type a question :) ");
+                    continue;
+                }
+
+
+                if (theQuestion.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                // Insult & Slur
+                if (IsInsultOrSlur(theQuestion)) return;
+        
+
+                // Get random number
+                int randomNum = randomObj.Next(4);
+
+                Console.ForegroundColor = (ConsoleColor)randomObj.Next(15);
+
+                // Random number to display answer
+                switch (randomNum)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("YES!");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("No!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Definitely NO!");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("You're awesome, YES!");
+                            break;
+                        }
+                }
             }
 
-            
+
             // Original color 
             Console.ForegroundColor = origColor;
         }
 
-        /// <summary>
-        /// Print the name of program and author
-        /// To console
-        /// </summary>
+        // Print the name of program and author to console
         static void ProgramInfo()
         {
             // Console text colors for program name
@@ -53,7 +100,6 @@ namespace ask_me_a_question
             Console.WriteLine("By: Dennis Luo ");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Type 'quit' anytime to leave program");
-
         }
 
         // Will return text user types
@@ -66,57 +112,26 @@ namespace ask_me_a_question
             string theQuestion = Console.ReadLine();
 
             return theQuestion;
-
         }
 
-        // Get answer for user
-        static void  GetAnswerFromUser(string theQuestion)
+        // Insults & Slurs
+        static bool IsInsultOrSlur(string theQuestion)
         {
-            if (theQuestion.Length == 0)
-            {
-                Console.WriteLine("Please type a question :) ");
-                return;
-            }
-            if (theQuestion.ToLower() == "quit")
-            {
-                break;
-            }
-
             // Insults & slurs will exit program
-            if (theQuestion.ToLower() == "you suck")
+            if (IsSlur(theQuestion))
             {
-                Console.WriteLine("You suck even more! Exiting program...BYE");
-                break;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Wow, why do you gotta be so mean! Exiting program...BYE");
+                return true;
             }
-            // Get random number
-            int randomNum = randomObj.Next(4);
+            return false;
+        }
 
-            // Random number to display answer
-            switch (randomNum)
-            {
-                case 0:
-                    {
-                        Console.WriteLine("YES!");
-                        break;
-                    }
-                case 1:
-                    {
-                        Console.WriteLine("No!");
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("Definitely NO!");
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.WriteLine("You're awesome, YES!");
-                        break;
-                    }
-            }
-
-
+        // Short array of insults...have fun reading..
+        static bool IsSlur(string theQuestion)
+        {
+            string[] slurs = new String[] { "you suck", "shut up", "you're fake"};
+            return slurs.Contains(theQuestion.ToLower());
         }
 
     }
